8baf9cd34e5ff84bf0b57e9b1962693206659542
Abuzar Mahmood <abuzarmahmood@gmail.com>
2024-06-17 19:52:52 -0400

    Prevent searching if no documents have been ocr_d

--- src/pages/Search.py
+++ src/pages/Search.py
@@ -28,98 +28,99 @@ df_path = os.path.join(base_path, 'doc_df.csv')
 # else:
 #     st.write("No documents uploaded yet.")
 
-############################################################
-# Search
-############################################################
-
 if s3.exists(df_path): 
   with s3.open(df_path, 'rb') as f:
       df = pd.read_csv(f)
   st.write("Document database loaded successfully.")
+
+  ############################################################
+  # Search
+  ############################################################
+
+  with st.form(key='search_form'):
+      st.write("Search for a word or phrase")
+      search_term = st.text_input("Search term", "")
+      submit_button = st.form_submit_button(label='Submit')
+
+  if submit_button:
+    # Iterate through each row in the dataframe
+    # and search for the search term in the 'words' column
+    search_results = []
+    for i, row in df.iterrows():
+      words = row['words']
+      decoded_words = [word.encode('utf-8').decode('utf-8') for word in words]
+      words_str = ''.join(decoded_words)
+      match_value = fuzz.partial_token_set_ratio(words_str, search_term)
+      search_results.append(match_value)
+
+    # Show top 'n' search results
+    n = 5
+    top_n_indices = np.argsort(search_results)[-n:][::-1]
+    st.write("Top search results:")
+    print_list = []
+    for idx in top_n_indices:
+      word_list = literal_eval(df.iloc[idx]['words'])
+      extracted_words = process.extract(
+          search_term, word_list, limit=5 
+          )
+      basename = os.path.basename(df.iloc[idx]['file_path'])
+      print_dict = {
+          'file_name': basename,
+          'file_type': df.iloc[idx]['file_type'],
+          'notes': df.iloc[idx]['notes'],
+          'words': str(extracted_words)
+          }
+      print_list.append(print_dict)
+      # print_str = pformat(print_dict) 
+      # st.write(print_str)
+    print_df = pd.DataFrame(print_list)
+    print(print_df)
+    # st.write(print_list)
+    st.write(print_df)
+
+  ############################################################
+  # Viewer
+  ############################################################
+
+  with st.form(key='view_form'):
+      st.write("Enter filename for file to view")
+      filename = st.text_input("Filename", "")
+      submit_button = st.form_submit_button(label='Submit')
+
+
+  if submit_button:
+    fin_path = os.path.join(data_path, filename)
+    if s3.exists(fin_path): 
+      pdf_content = s3.open(fin_path, 'rb').read()
+      pdf_viewer(pdf_content)
+      st.write("File loaded successfully.")
+    else:
+        st.write("File not found.")
+
+  ############################################################
+  # Downloader
+  ############################################################
+
+  with st.form(key='Download form'):
+      st.write("Enter filename for file to download")
+      filename = st.text_input("Filename", "")
+      submit_button = st.form_submit_button(label='Submit')
+
+
+  if submit_button:
+    fin_path = os.path.join(data_path, filename)
+    if s3.exists(fin_path): 
+      pdf_content = s3.open(fin_path, 'rb').read()
+      st.download_button(
+          label="Download file",
+          data=pdf_content,
+          file_name=filename,
+          # mime='application/pdf'
+          )
+      st.write("File loaded successfully.")
+    else:
+        st.write("File not found.")
+
 else:
     st.write("No OCR'd documents found.")
-
-with st.form(key='search_form'):
-    st.write("Search for a word or phrase")
-    search_term = st.text_input("Search term", "")
-    submit_button = st.form_submit_button(label='Submit')
-
-if submit_button:
-  # Iterate through each row in the dataframe
-  # and search for the search term in the 'words' column
-  search_results = []
-  for i, row in df.iterrows():
-    words = row['words']
-    decoded_words = [word.encode('utf-8').decode('utf-8') for word in words]
-    words_str = ''.join(decoded_words)
-    match_value = fuzz.partial_token_set_ratio(words_str, search_term)
-    search_results.append(match_value)
-
-  # Show top 'n' search results
-  n = 5
-  top_n_indices = np.argsort(search_results)[-n:][::-1]
-  st.write("Top search results:")
-  print_list = []
-  for idx in top_n_indices:
-    word_list = literal_eval(df.iloc[idx]['words'])
-    extracted_words = process.extract(
-        search_term, word_list, limit=5 
-        )
-    basename = os.path.basename(df.iloc[idx]['file_path'])
-    print_dict = {
-        'file_name': basename,
-        'file_type': df.iloc[idx]['file_type'],
-        'notes': df.iloc[idx]['notes'],
-        'words': str(extracted_words)
-        }
-    print_list.append(print_dict)
-    # print_str = pformat(print_dict) 
-    # st.write(print_str)
-  print_df = pd.DataFrame(print_list)
-  print(print_df)
-  # st.write(print_list)
-  st.write(print_df)
-
-############################################################
-# Viewer
-############################################################
-
-with st.form(key='view_form'):
-    st.write("Enter filename for file to view")
-    filename = st.text_input("Filename", "")
-    submit_button = st.form_submit_button(label='Submit')
-
-
-if submit_button:
-  fin_path = os.path.join(data_path, filename)
-  if s3.exists(fin_path): 
-    pdf_content = s3.open(fin_path, 'rb').read()
-    pdf_viewer(pdf_content)
-    st.write("File loaded successfully.")
-  else:
-      st.write("File not found.")
-
-############################################################
-# Downloader
-############################################################
-
-with st.form(key='Download form'):
-    st.write("Enter filename for file to download")
-    filename = st.text_input("Filename", "")
-    submit_button = st.form_submit_button(label='Submit')
-
-
-if submit_button:
-  fin_path = os.path.join(data_path, filename)
-  if s3.exists(fin_path): 
-    pdf_content = s3.open(fin_path, 'rb').read()
-    st.download_button(
-        label="Download file",
-        data=pdf_content,
-        file_name=filename,
-        # mime='application/pdf'
-        )
-    st.write("File loaded successfully.")
-  else:
-      st.write("File not found.")
 
